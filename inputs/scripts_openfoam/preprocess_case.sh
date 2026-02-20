92441e1179bb721a9d990fcaf57cf755aa8edfbc
Pablo <alonsoap@unican.es>
2026-02-20 02:28:27 +0100

    run case 8

--- inputs/scripts_openfoam/preprocess_case.sh
+++ inputs/scripts_openfoam/preprocess_case.sh
@@ -11,5 +11,3 @@ checkMesh # 2. Optional: check mesh quality
 setWaveParameters # 3. Set wave parameters (if needed, depends on your waveProperties)
 setWaveField # 4. Initialize wave field in 0/ folder
 decomposePar # 5. Decompose per processor
-
-#
